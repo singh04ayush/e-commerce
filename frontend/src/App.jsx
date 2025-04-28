df8bcc0924be85ea0f71dff0e8da7a78b56993e0
Ayush Singh <sayhi.ayush@gmail.com>
2025-04-28 22:47:18 +0530

    Navbar done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,13 @@ import Cart from './pages/Cart'
 import Orders from './pages/Orders'
 import PlaceOrder from './pages/PlaceOrder'
 import Login from './pages/Login'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
 
+      <Navbar />
       <Routes>
         <Route path='/' element={ <Home /> } />
         <Route path='/collection' element={ <Collection /> } />
