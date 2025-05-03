6bd32d737abcaedf550bcd2517b40d88deed9fba
Ayush <sayhi.ayush@gmail.com>
2025-05-03 23:26:55 +0530

    collection page done

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
-import { assets } from '../assets/assets';
+import { assets, products } from '../assets/assets';
 import Title from '../components/Title';
 import ProductItem from '../components/ProductItem';
 
@@ -10,7 +10,8 @@ const Collection = () => {
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
-  const [subcatoery, setSubCategory] = useState([]);
+  const [subCategory, setSubCategory] = useState([]);
+  const [sortType, setSortType] = useState('relavent');
 
   const toggleCategory = (e) =>{
     if(category.includes(e.target.value)){
@@ -20,15 +21,55 @@ const Collection = () => {
     }
   }
 
+  const toggleSubCategory = (e) =>{
+    if(subCategory.includes(e.target.value)){
+      setSubCategory(prev=> prev.filter(item=> item !== e.target.value))
+    }else{
+      setSubCategory(prev => [...prev, e.target.value])
+    }
+  }
+
+  const applyFilter = () =>{
+    let productsCopy = products.slice();
+
+    if(category.length > 0){
+      productsCopy = productsCopy.filter(item => category.includes(item.category));
+    }
+
+    if(subCategory.length > 0){
+      productsCopy = productsCopy.filter(item => subCategory.includes(item.subCategory));
+    }
+    setFilterProducts(productsCopy);
+
+  }
+
+  const sortProduct = () =>{
+    let fpCopy = filterProducts.slice();
+
+    switch(sortType){
+
+      case 'low-high':
+        setFilterProducts(fpCopy.sort((a,b)=>(a.price - b.price)));
+        break;
+      
+      case 'high-low':
+        setFilterProducts(fpCopy.sort((a,b)=>(b.price - a.price)))
+        break;
+
+      default:
+        applyFilter();
+        break;
+    }
+
+  }
 
   useEffect(()=>{
-    setFilterProducts(products)
-  }, [])
-
-  useEffect(()=>{
-    console.log(category);
-  },[category])
+    applyFilter();
+  },[category, subCategory])
   
+  useEffect(()=>{
+    sortProduct();
+  },[sortType])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
@@ -53,18 +94,18 @@ const Collection = () => {
           </div>
         </div>
 
-        {/* subcatoery */}  
+        {/* subCategory */}  
         <div className={`border border-gray-300 pl-5 py-3 mt-5 ${showFilter ? '' : 'hidden'} sm:block`}>
           <p className='mb-3 text-sm font-medium'>TYPE</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className='flex gap-2'>
-              <input type="checkbox" value={'Topwear'} className='w-3' />Topwear
+              <input type="checkbox" value={'Topwear'} className='w-3' onChange={toggleSubCategory} />Topwear
             </p>
             <p className='flex gap-2'>
-              <input type="checkbox" value={'Bottomwear'} className='w-3' />Bottomwear
+              <input type="checkbox" value={'Bottomwear'} className='w-3' onChange={toggleSubCategory} />Bottomwear
             </p>
             <p className='flex gap-2'>
-              <input type="checkbox" value={'Winterwear'} className='w-3' />Winterwear
+              <input type="checkbox" value={'Winterwear'} className='w-3' onChange={toggleSubCategory} />Winterwear
             </p>
           </div>
         </div>
@@ -77,7 +118,7 @@ const Collection = () => {
         <div className='flex justify-between text-base sm:text-2xl mb-4'>
           <Title text1={'ALL'} text2={'COLLECTION'} />
           {/* Product Sort */}
-          <select className='border-2 border-gray-300 text-sm px-2'>
+          <select onChange={(e)=>setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2'>
             <option value="relavent">Sort by: Relavent</option>
             <option value="low-high">Sort by: Low to High</option>
             <option value="high-low">Sort by: High to Low</option>
