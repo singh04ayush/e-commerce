e1d98c4f87556671e811e6e066faa80a5c04d67a
Ayush <sayhi.ayush@gmail.com>
2025-05-03 23:44:32 +0530

    search func done

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -6,7 +6,7 @@ import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
 
-  const { products } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
@@ -31,6 +31,10 @@ const Collection = () => {
 
   const applyFilter = () =>{
     let productsCopy = products.slice();
+
+    if(showSearch && search){
+      productsCopy = productsCopy.filter(item=>item.name.toLowerCase().includes(search.toLowerCase()));
+    }
 
     if(category.length > 0){
       productsCopy = productsCopy.filter(item => category.includes(item.category));
@@ -65,7 +69,7 @@ const Collection = () => {
 
   useEffect(()=>{
     applyFilter();
-  },[category, subCategory])
+  },[category, subCategory, search, showSearch])
   
   useEffect(()=>{
     sortProduct();
