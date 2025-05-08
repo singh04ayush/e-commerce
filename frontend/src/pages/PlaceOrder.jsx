d0c067261b6398607ee144a4a561e0c4460675e3
Ayush <sayhi.ayush@gmail.com>
2025-05-08 21:59:15 +0530

    working on order page

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Title from '../components/Title'
 
 const PlaceOrder = () => {
   return (
-    <div>
-      placeorder
+    <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
+
+      {/* Left Side */}
+      <div className='flex flex-col gap-4 w-full sm:max-w-[480px]'>
+
+        <div className='text-xl sm:text-2xl my-3'>
+          <Title text1={'Delivery'} text2={'Information'} />
+        </div>
+
+        <div className='flex gap-3'>
+          <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='First Name' />
+          <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Last Name' />
+        </div>
+
+      </div>
     </div>
   )
 }
