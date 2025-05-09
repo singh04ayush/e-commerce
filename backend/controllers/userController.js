cfd0e822db5adbb7d75006f017d7aeeafd0aff73
Ayush <sayhi.ayush@gmail.com>
2025-05-10 00:09:51 +0530

    login api done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,7 +9,31 @@ const createToken = (id) =>{
 
 //Route for User Login
 const loginUser = async (req, res) => {
-    
+    try {
+        
+        const {email, password} = req.body;
+
+        const user = await userModel.findOne({email});
+
+        if(!user){
+            return res.json({success:false, message: "User does not Exist"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(isMatch){
+
+            const token = createToken(user._id);
+            res.json({success: true, token});
+        }
+        else{
+            res.json({success: false, message: "Invalid Credentials"});
+        }
+
+    } catch (error) {
+         console.log(error);
+        res.json({success: false, message: error.message});
+    }
 }
 
 
