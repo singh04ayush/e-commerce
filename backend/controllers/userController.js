0a3695bf7f193ae83a860142f1cbbbc49da598c4
Ayush <sayhi.ayush@gmail.com>
2025-05-09 23:41:51 +0530

    working on api

---
+++ backend/controllers/userController.js
@@ -0,0 +1,30 @@
+
+
+
+//Route for User Login
+const loginUser = async (req, res) => {
+    
+}
+
+
+
+
+
+
+//Route for user Regsitration
+const registerUser = async (req, res) =>{
+    res.json({msg:"register api working"});
+}
+
+
+
+
+// Route for admin Login
+const adminLogin = async (req, res) => {
+    
+}
+
+
+
+
+export {loginUser, registerUser, adminLogin}
