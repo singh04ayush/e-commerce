dcf983fe24a5b9207dad34b082d3c69c452f0ba8
Ayush <sayhi.ayush@gmail.com>
2025-05-11 02:37:40 +0530

    addProduct done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,6 @@
+import {v2 as cloudinary} from 'cloudinary';
+import productModel from '../models/productModel.js';
+
 
 
 // function for add product
@@ -6,20 +9,62 @@ const addProduct = async (req, res) => {
     try{
         const {name, description, price, category, subCategory, sizes, bestseller} = req.body;
 
-        const image1 = req.files.image1[0];
-        const image2 = req.files.image2[0];
-        const image3 = req.files.image3[0];
-        const image4 = req.files.image4[0];
+        const image1 = req.files.image1 && req.files.image1[0];
+        const image2 = req.files.image2 && req.files.image2[0];
+        const image3 = req.files.image3 && req.files.image3[0];
+        const image4 = req.files.image4 && req.files.image4[0];
 
-        console.log(name, description, price, category, subCategory, sizes, bestseller);
-        console.log(image1, image2, image3, image4);
-        res.json({})
+        const images = [image1, image2, image3, image4].filter((item)=> item !== undefined);
+
+        let imagesUrl = await Promise.all(
+            images.map(async (item) => {
+                let result = await cloudinary.uploader.upload(item.path, {resource_type:'image'});
+                return result.secure_url
+            })
+        )
+
+        const productData = {
+            name,
+            description,
+            category,
+            price: Number(price),
+            subCategory,
+            bestseller: bestseller === "true" ? true: false,
+            sizes: JSON.parse(sizes),
+            image: imagesUrl,
+            date: Date.now()
+        }
+
+        console.log(productData);
+
+        const product = new productModel(productData);
+        await product.save();
+        
+        res.json({success: true, message: "Product added"});
+
     } catch (error){
         console.log(error);
         res.json({success:false, message:error.message});
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -40,10 +85,39 @@ const listProducts = async (req, res) => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // function for removing product
 const removeProduct = async (req, res) => {
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
