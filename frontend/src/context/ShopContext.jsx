21956a71624c9a9ef5d1db3249864123574ec5b7
Ayush <sayhi.ayush@gmail.com>
2025-05-19 16:10:15 +0530

    func added

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -105,6 +105,12 @@ const ShopContextProvider = (props) => {
         getProductsData();
     },[])
 
+    useEffect(()=>{
+        if(!token && localStorage.getItem('token')){
+            setToken(localStorage.getItem('token'));
+        }
+    },[])
+
     const value = {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch, cartItems, addToCart,
