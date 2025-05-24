d227da8120a5f7088b548987aee2761dca1585e6
Ayush <sayhi.ayush@gmail.com>
2025-05-24 15:58:57 +0530

    assests fixed

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
-import { assets, products } from '../assets/assets';
+import { assets} from '../assets/assets';
 import Title from '../components/Title';
 import ProductItem from '../components/ProductItem';
 
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
-import { assets, products } from '../assets/assets';
+import { assets } from '../assets/assets';
 import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
